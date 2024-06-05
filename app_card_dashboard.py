567952ec09bd54949010429e2f416cd62ef36f13
srector8 <157165984+srector8@users.noreply.github.com>
2024-06-05 09:29:45 -0400

    Update app_card_dashboard.py

--- app_card_dashboard.py
+++ app_card_dashboard.py
@@ -15,7 +15,11 @@ from datetime import datetime
 def main():
     st.title("App Card Dashboard")
 
-  
+    # Function to load data
+    def load_data(file):
+        df = pd.read_csv(file)
+        return df
+
     # Function to transform title
     def transform_title(title):
         if pd.isnull(title):
