3e1933176df4e3bd369e25fcbd799bf60063015f
srector8 <157165984+srector8@users.noreply.github.com>
2024-08-13 14:59:50 -0400

    Update app_card_dashboard.py

--- app_card_dashboard.py
+++ app_card_dashboard.py
@@ -10,34 +10,6 @@ Original file is located at
 import pandas as pd
 import altair as alt
 import streamlit as st
-import snowflake.connector
-
-# Snowflake connection details
-snowflake_account = 'MOHEGAN-EDW'
-snowflake_user = 'SRECTOR@MOHEGANSUN.COM'
-snowflake_password = '---!'
-snowflake_database = 'MOHEGAN_CTSUN_PROD_DB'
-snowflake_schema = 'CT_SUN_APPS_FEED'
-snowflake_warehouse = 'MSCT_CTSUN_WH_XS'
-snowflake_role = 'SUN-APP-EDW-CTSUN'
-
-# Establish Snowflake connection
-def get_snowflake_connection():
-   return snowflake.connector.connect(
-        user=snowflake_user,
-        password=snowflake_password,
-        account=snowflake_account,
-        database=snowflake_database,
-        schema=snowflake_schema,
-        warehouse=snowflake_warehouse,
-        role=snowflake_role
-    )
-# Function to load data from Snowflake
-def load_data_from_snowflake(query):
-    conn = get_snowflake_connection()
-    df = pd.read_sql(query, conn)
-    conn.close()
-    return df
 
 def main():
     st.title("App Card Dashboard")
@@ -47,125 +19,118 @@ def main():
     st.write('Exposure Rating - The ratio of impressions to unique impressions')
     st.write('Ticks mark gamedays')
 
-    # Query to get data from Snowflake
-    query = """
-    SELECT 
-        TITLE, 
-        CLICKS, 
-        IMPRESSIONS, 
-        CLICKTHROUGH_RATE_PERCENT, 
-        UNIQUE_IMPRESSIONS, 
-        UNIQUE_CLICKS, 
-        UNIQUE_CLICKTHROUGH_RATE_PERCENT, 
-        EXPOSURE_RATING, 
-        UTILITY_RATING, 
-        CREATE_TIMESTAMP
-    FROM 
-        CARDS_WNBA_CON
-    """
+    # Function to load data
+    def load_data(file):
+        df = pd.read_csv(file)
+        return df
 
-    df = load_data_from_snowflake(query)
-
-    # Convert 'CREATE_TIMESTAMP' to datetime and extract date
-    df['date'] = pd.to_datetime(df['CREATE_TIMESTAMP']).dt.date
-
-    # Renaming KPI's
-    df.rename(columns={
-        'CLICKS': 'Clicks',
-        'IMPRESSIONS': 'Impressions',
-        'CLICKTHROUGH_RATE_PERCENT': 'Click-Through Rate Percent',
-        'UNIQUE_IMPRESSIONS': 'Unique Impressions',
-        'UNIQUE_CLICKS': 'Unique Clicks',
-        'UNIQUE_CLICKTHROUGH_RATE_PERCENT': 'Unique Click-Through Rate Percent',
-        'EXPOSURE_RATING': 'Exposure Rating',
-        'UTILITY_RATING': 'Utility Rating'
-    }, inplace=True)
-
-    # Apply the title transformation
+    # Function to transform title
     def transform_title(title):
         if pd.isnull(title):
             return title
         else:
             return title.lower().title()
 
-    df['TITLE'] = df['TITLE'].apply(transform_title)
+    # Streamlit file uploader
+    uploaded_file = 'App_Card_Data_0527_with_dates.csv'
 
-    # Filter titles with less than 20 date entries
-    title_counts = df['TITLE'].value_counts()
-    valid_titles = title_counts[title_counts >= 20].index.tolist()
-    df = df[df['TITLE'].isin(valid_titles)]
+    if uploaded_file is not None:
+        df = load_data(uploaded_file)
 
-    # List of card titles for dropdown
-    card_titles = df['TITLE'].unique()
+        # Convert 'DATE' column to datetime if it's not already
+        df['date'] = pd.to_datetime(df['date'])
 
-    # Streamlit selectbox for card titles
-    card_title = st.selectbox('Select a card:', card_titles)
+        # Renaming KPI's
+        df.rename(columns={
+            'CLICKS': 'Clicks',
+            'IMPRESSIONS': 'Impressions',
+            'CLICKTHROUGH_RATE_PERCENT': 'Click-Through Rate Percent',
+            'UNIQUE_IMPRESSIONS': 'Unique Impressions',
+            'UNIQUE_CLICKS': 'Unique Clicks',
+            'UNIQUE_CLICKTHROUGH_RATE_PERCENT': 'Unique Click-Through Rate Percent',
+            'EXPOSURE_RATING': 'Exposure Rating',
+            'UTILITY_RATING': 'Utility Rating'
+        }, inplace=True)
 
-    # List of KPIs
-    kpis = [
-        "Clicks", "Impressions", "Click-Through Rate Percent",
-        "Unique Impressions", "Unique Clicks",
-        "Unique Click-Through Rate Percent", "Exposure Rating", "Utility Rating"
-    ]
+        # Apply the title transformation
+        df['TITLE'] = df['TITLE'].apply(transform_title)
 
-    # Streamlit selectbox for KPIs
-    selected_kpi = st.selectbox('Select a KPI:', kpis)
+        # Filter titles with less than 20 date entries
+        title_counts = df['TITLE'].value_counts()
+        valid_titles = title_counts[title_counts >= 20].index.tolist()
+        df = df[df['TITLE'].isin(valid_titles)]
 
-    # Function to plot time series
-    def plot_time_series(card_title, selected_kpi):
-        card_data = df[df['TITLE'] == card_title]
+        # List of card titles for dropdown
+        card_titles = df['TITLE'].unique()
 
-        # Define the base chart
-        base_chart = alt.Chart(card_data).mark_line().encode(
-            x=alt.X('date:T', title='Date', axis=alt.Axis(format='%m/%d')),
-            y=alt.Y(selected_kpi, title=selected_kpi.capitalize()),
-            tooltip=['date:T', selected_kpi]
-        ).properties(
-            title=f"{selected_kpi} Time-Series for {card_title}",
-            width=800,
-            height=400
-        )
+        # Streamlit selectbox for card titles
+        card_title = st.selectbox('Select a card:', card_titles)
 
-        # Define the dates and colors for vertical lines
-        important_dates = pd.DataFrame({
-            'date': pd.to_datetime(['2024-05-14', '2024-05-17', '2024-05-23', '2024-05-28', '2024-05-31', '2024-06-04', '2024-06-08', '2024-06-10']),  # Add more dates as needed
-            'color': ['red', 'red', 'red', 'red', 'red', 'red', 'red', 'red']  # Add more colors corresponding to dates
-        })
+        # List of KPIs
+        kpis = [
+            "Clicks", "Impressions", "Click-Through Rate Percent",
+            "Unique Impressions", "Unique Clicks",
+            "Unique Click-Through Rate Percent", "Exposure Rating", "Utility Rating"
+        ]
 
-        rules = alt.Chart(important_dates).mark_rule().encode(
-            x='date:T',
-            color=alt.Color('color:N', scale=None)
-        )
+        # Streamlit selectbox for KPIs
+        selected_kpi = st.selectbox('Select a KPI:', kpis)
 
-        # Combine the base chart with the rules
-        final_chart = base_chart + rules
+        # Function to plot time series
+        def plot_time_series(card_title, selected_kpi):
+            card_data = df[df['TITLE'] == card_title]
 
-        st.altair_chart(final_chart.interactive())
+            # Define the base chart
+            base_chart = alt.Chart(card_data).mark_line().encode(
+                x=alt.X('date:T', title='Date', axis=alt.Axis(format='%m/%d')),
+                y=alt.Y(selected_kpi, title=selected_kpi.capitalize()),
+                tooltip=['date:T', selected_kpi]
+            ).properties(
+                title=f"{selected_kpi} Time-Series for {card_title}",
+                width=800,
+                height=400
+            )
 
-        # Calculate and display pre, post, and game day averages
-        pre_game_values = []
-        game_day_values = []
-        post_game_values = []
+            # Define the dates and colors for vertical lines
+            important_dates = pd.DataFrame({
+                'date': pd.to_datetime(['2024-05-14', '2024-05-17', '2024-05-23', '2024-05-28', '2024-05-31', '2024-06-04', '2024-06-08', '2024-06-10']),  # Add more dates as needed
+                'color': ['red', 'red', 'red', 'red', 'red', 'red', 'red', 'red']  # Add more colors corresponding to dates
+            })
 
-        for game_date in important_dates['date']:
-            pre_game_date = game_date - pd.Timedelta(days=1)
-            post_game_date = game_date + pd.Timedelta(days=1)
+            rules = alt.Chart(important_dates).mark_rule().encode(
+                x='date:T',
+                color=alt.Color('color:N', scale=None)
+            )
 
-            pre_game_values.append(card_data[card_data['date'] == pre_game_date][selected_kpi].mean())
-            game_day_values.append(card_data[card_data['date'] == game_date][selected_kpi].mean())
-            post_game_values.append(card_data[card_data['date'] == post_game_date][selected_kpi].mean())
+            # Combine the base chart with the rules
+            final_chart = base_chart + rules
 
-        pre_game_mean = pd.Series(pre_game_values).mean()
-        game_day_mean = pd.Series(game_day_values).mean()
-        post_game_mean = pd.Series(post_game_values).mean()
+            st.altair_chart(final_chart.interactive())
 
-        st.write(f"The mean {selected_kpi.lower()} one day before game days is: {pre_game_mean}")
-        st.write(f"The mean {selected_kpi.lower()} on game days is: {game_day_mean}")
-        st.write(f"The mean {selected_kpi.lower()} one day after game days is: {post_game_mean}")
+            # Calculate and display pre, post, and game day averages
+            pre_game_values = []
+            game_day_values = []
+            post_game_values = []
 
-    # Plot the time series for the selected card title and KPI
-    if card_title and selected_kpi:
-        plot_time_series(card_title, selected_kpi)
+            for game_date in important_dates['date']:
+                pre_game_date = game_date - pd.Timedelta(days=1)
+                post_game_date = game_date + pd.Timedelta(days=1)
+
+                pre_game_values.append(card_data[card_data['date'] == pre_game_date][selected_kpi].mean())
+                game_day_values.append(card_data[card_data['date'] == game_date][selected_kpi].mean())
+                post_game_values.append(card_data[card_data['date'] == post_game_date][selected_kpi].mean())
+
+            pre_game_mean = pd.Series(pre_game_values).mean()
+            game_day_mean = pd.Series(game_day_values).mean()
+            post_game_mean = pd.Series(post_game_values).mean()
+
+            st.write(f"The mean {selected_kpi.lower()} one day before game days is: {pre_game_mean}")
+            st.write(f"The mean {selected_kpi.lower()} on game days is: {game_day_mean}")
+            st.write(f"The mean {selected_kpi.lower()} one day after game days is: {post_game_mean}")
+
+        # Plot the time series for the selected card title and KPI
+        if card_title and selected_kpi:
+            plot_time_series(card_title, selected_kpi)
 
 if __name__ == "__main__":
     main()
